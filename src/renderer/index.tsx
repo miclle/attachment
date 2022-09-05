2b93a5d2ab1b41914329641042db51787adc4421
Miclle Zheng <miclle.zheng@gmail.com>
2022-09-05 11:17:39 +0800

    Enable react strict mode

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,9 +1,16 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
+
 import App from './App';
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!;
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // calling IPC exposed from preload script
 window.electron.ipcRenderer.once('ipc-example', (arg) => {
